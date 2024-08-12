a445c218c6a9f41ab57afae2d5780cca3ea9daf5
Agam Riyandana <87102026+agam-riyandana@users.noreply.github.com>
2024-08-13 04:37:15 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Banner from "@/components/Banner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -30,6 +31,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Banner />
       <body className={inter.className}>{children}</body>
     </html>
   );
