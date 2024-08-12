a9652f582082b9778765e0453cb4ee42dae4599e
Agam Riyandana <87102026+agam-riyandana@users.noreply.github.com>
2024-08-12 20:50:29 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,15 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Bikin Twibbon",
+  description: "Mari rayakan HUT RI ke-79 dengan semangat Nusantara Baru Indonesia Maju. Buat Twibbon kamu sekarang dan tunjukkan cinta untuk tanah air!",
+  generator: 'Next.js',
+  manifest: 'https://agamriyandana.my.id/manifest.json',
+  applicationName: 'Next.js',
+  referrer: 'origin-when-cross-origin',
+  keywords: "agam, agamriyandana, my.id, domain Indonesia, Jember, pandi, portofolio, domain nusantara, digital, website, internasional, asia, eropa",
+  authors: [{ name: 'Agam' }, { name: 'Riyandana', url: 'https://agamriyandana.my.id' }],
+  creator: 'Agam Riyandana',
+  publisher: 'Agam Riyandana',
 };
 
 export const viewport: Viewport = {
