1b7a4b13ec87e70184b4506c9e658ee1126f367a
Agam Riyandana <87102026+agam-riyandana@users.noreply.github.com>
2024-08-12 21:03:09 +0700

    Update not-found.js

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,14 +1,11 @@
-import { AlertTriangle } from "lucide-react";
 import Link from 'next/link'
  
-export default function Page() {
-    return (
-        <div className="z-10 bg-background h-full w-full absolute top-0 left-0 right-0 flex items-center justify-center">
-            <div className="grid gap-2 text-center max-w-xs px-6 place-items-center">
-                <AlertTriangle className="h-5 w-5" />
-                <p className="text-muted-foreground text-sm">The page you are trying to access does not exists.</p>
-                <Button variant="outline" asChild><Link href="/">Back to Homepage</Link></Button>
-            </div>
-        </div>
-    )
+export default function NotFound() {
+  return (
+    <div>
+      <h2>Not Found</h2>
+      <p>Could not find requested resource</p>
+      <Link href="/">Return Home</Link>
+    </div>
+  )
 }
