24a0ab5b052f265889bc2e0ede9776524292985d
Agam Riyandana <87102026+agam-riyandana@users.noreply.github.com>
2024-08-13 04:26:23 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,7 +30,6 @@ export default function Home({ searchParams }: Readonly<Props>) {
         <h1 className="text-[22px] sm:text-[36px] font-bold leading-[130%] mb-[18px]">
           <span className="text-primary-500 text-[40px] sm:text-[50px]">
             HUT ke-79
-            <Banner />
             <br />
           </span>{" "}
           Nusantara Baru Indonesia Maju
